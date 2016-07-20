36e95842891bf3a98b8d10ad16ca7c8df4cecb67
Rahul Roshan <rahul@Administrators-MacBook-Pro-3.local>
2016-07-20 13:17:20 +0530

    generators, promise

--- node.js/promise.js
+++ node.js/promise.js
@@ -3,9 +3,9 @@ function dieToss() {
 }
 
 console.log('1');
-var promise = new RSVP.Promise(function(fulfill, reject) {
+var promise = new Promise(function(fullfill, reject) { // fullfill and reject order matters in case of promise.then function 
   var n = dieToss();
-  if (n === 6) {
+  if (n == 6) {
     fulfill(n);
   } else {
     reject(n);
@@ -13,9 +13,13 @@ var promise = new RSVP.Promise(function(fulfill, reject) {
   console.log('2');
 });
 
-promise.then(function(toss) {
+promise.then(function(toss) { // promise.then ( onfullfilled, onrejected)
   console.log('Yay, threw a ' + toss + '.');  
 }, function(toss) {
   console.log('Oh, noes, threw a ' + toss + '.');  
 });
 console.log('3');
+
+
+// Note: if n == 6 then it would be a fullfill for the promise function, 
+//https://www.toptal.com/javascript/javascript-promises
