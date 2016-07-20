d8f277dce8f891158814d13c06ae09d1ddce1527
Rahul Roshan <rahul@Administrators-MacBook-Pro-3.local>
2016-07-20 13:19:26 +0530

    generator

--- node.js/generator.js
+++ node.js/generator.js
@@ -65,3 +65,19 @@ function onerror(err) {
 
 // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
 
+
+/*
+
+The yieldable objects currently supported are:
+
+promises
+thunks (functions)
+array (parallel execution)
+objects (parallel execution)
+generators (delegation)
+generator functions (delegation)
+Nested yieldable objects are supported, meaning you can nest promises within objects within arrays, and so on!
+
+*/
+
+
